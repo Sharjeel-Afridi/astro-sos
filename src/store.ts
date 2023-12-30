cc63ceb5a4b1c6f6216cbf0344467d447b04e691
RayyanNafees <nafees.rayyan@gmail.com>
2023-12-30 11:36:39 +0530

    Removed all the UI components, Not needed, just static seems enough

---
+++ src/store.ts
@@ -0,0 +1,18 @@
+import { map } from 'nanostores'
+export const state = map({
+  step: 1,
+  one: '',
+  two: '',
+  more: '',
+  type: '',
+  small: '',
+  middle: '',
+  large: '',
+  when: '',
+  position: '',
+  good: '',
+  anxious: '',
+  aggressive: '',
+  wounded: '',
+  unknown: '',
+})
