5d9ab56b8ec380d7b846f1f80955a67025f8ee9d
RayyanNafees <nafees.rayyan@gmail.com>
2024-02-15 21:57:58 +0530

    Added client

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,12 +1,14 @@
 import { initializeApp } from "firebase/app";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "my-public-api-key",
-  authDomain: "my-auth-domain",
-  projectId: "my-project-id",
-  storageBucket: "my-storage-bucket",
-  messagingSenderId: "my-sender-id",
-  appId: "my-app-id",
+  apiKey: "AIzaSyAQu96F7JwPu7MRVTDFvJWE4Z82gCdtdvs",
+  authDomain: "astro-sos.firebaseapp.com",
+  projectId: "astro-sos",
+  storageBucket: "astro-sos.appspot.com",
+  messagingSenderId: "66213182291",
+  appId: "1:66213182291:web:4545cd964440b56578ffa6",
+  measurementId: "G-10XQQZV1LQ"
 };
 
 export const app = initializeApp(firebaseConfig);
